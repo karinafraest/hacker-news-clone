d67be8ba6c91b06b66fca74f566c951559cf6985
Karina Franco <email": null>
2017-05-08 14:58:47 -0500

    user can edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,22 @@ class CommentsController < ApplicationController
     redirect_to post_path(@comment.post)
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@comment.post)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def comment_params
-    params.require(:comment).permit(:author, :description)
+    params.require(:comment).permit(:author, :description, :article)
   end
 end
