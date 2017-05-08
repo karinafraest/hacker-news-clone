3fe51280487d2845b5ef938e309692c7d1192aea
Karina Franco <email": null>
2017-05-08 12:03:36 -0500

    user can create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,29 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if helpers.current_user
+      @post = Post.new(post_params)
+      @post.author = helpers.current_user
+      if @post.save
+        redirect_to posts_path
+      else
+        render 'new'
+      end
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :author_id)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id
+
+  validates :title, presence: true
+  validates :url, presence: true
 end
