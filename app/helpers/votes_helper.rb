a9a70aed39d9dca493ebfef06cada59c27a120aa
Karina Franco <email": null>
2017-05-08 18:32:56 -0500

    went down a bug hunt and fixed votes

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,6 +1,10 @@
 module VotesHelper
 
   def current_vote(post)
-    @vote ||= Vote.find_by(author: current_user, post: post)
+    if current_user
+      @vote ||= Vote.find_by(author: current_user, post: post)
+    else
+      @vote ||= Vote.last
+    end
   end
 end
