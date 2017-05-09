86e5c34edd0da7470f4cf4b0fbcfa941f469b60d
Karina Franco <karinafraest@gmail.com>
2017-05-08 21:28:28 -0500

    routes microchallenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :posts do
+  post '/users', to: 'users#create', as: :users
+  get '/users/new', to: 'users#new', as: :new_user
+  post '/sessions', to: 'sessions#create', as: :sessions
+  get '/sessions/new', to: 'sessions#new', as: :new_session
+  delete '/sessions/:id', to: 'sessions#destroy', as: :session
+  
+  get '/posts', to: 'posts#index', as: :posts
+  post '/posts', to: 'posts#create'
+  get '/posts/new', to: 'posts#new', as: :new_post
+  get '/posts:id/edit', to: 'posts#edit', as: :edit_post
+  get '/posts/:id', to: 'posts#show', as: :post
+  patch '/posts/:id', to: 'posts#update'
+  put '/posts/:id', to: 'posts#update'
+  delete '/posts/:id', to: 'posts#destroy'
+
+  resources :posts, only: [] do
     resources :comments, except: [:destroy, :index]
     resources :votes, only: [:create, :destroy]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20170508212826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.string   "body"
-    t.integer  "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string   "description"
     t.integer  "author_id"
